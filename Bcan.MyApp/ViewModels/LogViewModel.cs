7ff5befd3951367effdbf8325e4fa9b9d9d066be
Burak Can <bc913dev@outlook.com>
2025-04-22 18:14:34 -0400

    Filter logs

--- Bcan.MyApp/ViewModels/LogViewModel.cs
+++ Bcan.MyApp/ViewModels/LogViewModel.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using Avalonia.Threading;
+using System.Linq;
 using Bcan.MyApp.Contracts;
 using Bcan.MyApp.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Bcan.MyApp.ViewModels;
 
@@ -11,12 +13,17 @@ public partial class LogViewModel : ObservableObject
 {
     private readonly ILoggerService _logger;
     public ObservableCollection<LogEntry> Logs { get; } = new();
+    public ObservableCollection<LogEntry> FilteredLogs { get; } = new();
+    
 
     [ObservableProperty]
-    private string _searchText = string.Empty;
+    private string? _searchText;
 
     [ObservableProperty]
-    private LogLevel? _filterLevel = null;
+    private string? _filterLevel;
+    
+    [ObservableProperty]
+    private IEnumerable<string> _logLevels = Enum.GetNames<LogLevel>();
 
 #pragma warning disable CS8618, CS9264
     public LogViewModel()
@@ -32,8 +39,18 @@ public partial class LogViewModel : ObservableObject
         {
             Avalonia.Threading.Dispatcher.UIThread.Post(() =>
             {
-                if (IsMatch(e)) Logs.Add(e);
+                //if (IsMatch(e))
+                Logs.Add(e);
+                ApplyFilter();
             });
+        };
+        
+        PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == nameof(FilterLevel) || e.PropertyName == nameof(SearchText))
+            {
+                ApplyFilter();
+            }
         };
     }
 
@@ -42,9 +59,48 @@ public partial class LogViewModel : ObservableObject
         if (!string.IsNullOrWhiteSpace(SearchText) && !entry.Message.Contains(SearchText, StringComparison.OrdinalIgnoreCase))
             return false;
 
-        if (FilterLevel.HasValue && entry.Level != FilterLevel.Value)
+        // if (FilterLevel.HasValue && entry.Level != FilterLevel.Value)
+        //     return false;
+
+        if (!Enum.TryParse<LogLevel>(FilterLevel, out var filterLev))
             return false;
 
+        if (filterLev != entry.Level)
+            return false;
+        
         return true;
+    }
+    
+    private void ApplyFilter()
+    {
+        FilteredLogs.Clear();
+        var filtered = Logs.AsEnumerable();
+
+        // if (FilterLevel is not null)
+        //     filtered = filtered.Where(log => log.Level == FilterLevel);
+        
+        if (!string.IsNullOrWhiteSpace(FilterLevel) &&
+            Enum.TryParse<LogLevel>(FilterLevel, out var level))
+        {
+            filtered = filtered.Where(log => log.Level == level);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(SearchText))
+        {
+            filtered = filtered.Where(log =>
+                log.Message.Contains(SearchText, StringComparison.OrdinalIgnoreCase));
+        }
+
+        foreach (var entry in filtered)
+        {
+            FilteredLogs.Add(entry);
+        }
+    }
+    
+    [RelayCommand]
+    public void ClearLogs()
+    {
+        Logs.Clear();
+        FilteredLogs.Clear();
     }
 }
