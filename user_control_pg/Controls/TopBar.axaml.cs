d0e72f237aba19933d6fbcefbb9857cac37f35ac
Burak Can <bc913dev@outlook.com>
2025-05-06 10:32:45 -0400

    ADd topbar

---
+++ user_control_pg/Controls/TopBar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Bcan.Uctrls.Pg.Controls;
+
+public partial class TopBar : UserControl
+{
+    public TopBar()
+    {
+        InitializeComponent();
+    }
+}
