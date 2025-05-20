3608e697d48bda2a993e561356e13502af780586
Burak Can <bc913dev@outlook.com>
2025-05-19 22:21:48 -0400

    Move runners to their own classes

---
+++ persistence_pg/Console/DapperRunner.cs
@@ -0,0 +1,51 @@
+public static class DapperRunner
+{
+    public static void Run(string dbPath)
+    {
+        var dbHandler = new DapperPersistenceService<Stock>(dbPath);
+        dbHandler.Init();
+        var stock1 = new Stock
+        {
+            Symbol = "MSFT"
+        };
+
+        var stock2 = new Stock
+        {
+            Symbol = "ANSS"
+        };
+
+        dbHandler.Post(stock1);
+        dbHandler.Post(stock2);
+
+
+        var stocks = dbHandler.GetAll();
+        foreach(var stock in stocks)
+        {
+            Console.WriteLine($"Id: {stock.Id} | Symbol: {stock.Symbol}");
+        }
+    }
+
+    public static async Task RunAsync(string dbPath)
+    {
+        var dbHandler = new DapperPersistenceService<Stock>(dbPath);
+        await dbHandler.InitAsync();
+        var stock1 = new Stock
+        {
+            Symbol = "MSFT"
+        };
+
+        var stock2 = new Stock
+        {
+            Symbol = "ANSS"
+        };
+
+        await dbHandler.PostAsync(stock1);
+        await dbHandler.PostAsync(stock2);
+
+        var stocksAsync = await dbHandler.GetAllAsync();
+        foreach(var stock in stocksAsync)
+        {
+            Console.WriteLine($"Id: {stock.Id} | Symbol: {stock.Symbol}");
+        }
+    }
+}
