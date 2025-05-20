055fca8f91403d79dbfd6039494fdb05c3e7366e
Burak Can <bc913dev@outlook.com>
2025-05-19 22:22:27 -0400

    Dont forget Sqliterunner

--- persistence_pg/Console/SQLiteRunner.cs
+++ persistence_pg/Console/SQLiteRunner.cs
@@ -1,6 +1,50 @@
-namespace DefaultNamespace;
 
-public class SQLiteRunner
+public static class SQLiteRunner
 {
-    
+    public static void Run(string dbPath)
+    {
+        var dbHandler = new SqlitePersistenceService<Stock>(dbPath);
+        var stock1 = new Stock
+        {
+            Symbol = "MSFT"
+        };
+
+        var stock2 = new Stock
+        {
+            Symbol = "ANSS"
+        };
+
+        dbHandler.Post(stock1);
+        dbHandler.Post(stock2);
+
+
+        var stocks = dbHandler.GetAll();
+        foreach(var stock in stocks)
+        {
+            Console.WriteLine($"Id: {stock.Id} | Symbol: {stock.Symbol}");
+        }
+    }
+
+    public static async Task RunAsync(string dbPath)
+    {
+        var dbHandler = new SqlitePersistenceService<Stock>(dbPath);
+        var stock1 = new Stock
+        {
+            Symbol = "MSFT"
+        };
+
+        var stock2 = new Stock
+        {
+            Symbol = "ANSS"
+        };
+
+        await dbHandler.PostAsync(stock1);
+        await dbHandler.PostAsync(stock2);
+
+        var stocksAsync = await dbHandler.GetAllAsync();
+        foreach(var stock in stocksAsync)
+        {
+            Console.WriteLine($"Id: {stock.Id} | Symbol: {stock.Symbol}");
+        }
+    }
 }
